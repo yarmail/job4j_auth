a91dd062513dac4f382650c10f45fcc18cba6949
yarmail <yarmail@yandex.ru>
2023-01-15 20:32:22 +0300

    1. RESTFull. Описание архитектуры [#6884 #380992]

--- src/main/java/ru/job4j/auth/Job4jAuthApplication.java
+++ src/main/java/ru/job4j/auth/Job4jAuthApplication.java
@@ -2,11 +2,18 @@ package ru.job4j.auth;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
 public class Job4jAuthApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(Job4jAuthApplication.class, args);
+    }
+
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
